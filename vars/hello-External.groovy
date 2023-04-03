ab40595ee090d098a351b9dc8bff5a8eac7928c4
RotemK1 <kalmanrotem@gmail.com>
2023-04-03 15:32:14 +0300

    update

--- vars/hello-External.groovy
+++ vars/hello-External.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]){
-    loadScript(name: 'hello-world.sh')
-    sh "./hello-world.sh ${config.name}"
+    sh "echo hello ${config.name}"
+    // loadScript(name: 'hello-world.sh')
+    // sh "./hello-world.sh ${config.name}"
 }
