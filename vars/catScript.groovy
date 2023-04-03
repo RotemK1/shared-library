a53a749a8b6baff1cc23e00f65301e9c76615722
RotemK1 <kalmanrotem@gmail.com>
2023-04-03 15:48:40 +0300

    ta

--- vars/catScript.groovy
+++ vars/catScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]){
     def scriptcontents = libraryResource "org/${config.name}"
     scriptcontents.replace('<FIRST>',"${config.name}")
+    sh "ls"
     sh "cat scriptcontents"
 }
