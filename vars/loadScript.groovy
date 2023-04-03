cc7c3d292364990c4012edf8bbafb3bef92f204f
RotemK1 <kalmanrotem@gmail.com>
2023-04-03 15:42:18 +0300

    updat

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,5 +1,7 @@
 def call(Map config = [:]){
     def scriptcontents = libraryResource "org/${config.name}"
-    writeFile file: "${config.name}", text: scriptcontents
-    sh "chmod a+x ./${config.name}"
+    template.replace('<FIRST>',"${config.name}")
+    sh "cat scriptcontents"
+    //writeFile file: "${config.name}", text: scriptcontents
+    // sh "chmod a+x ./${config.name}"
 }
