1ea2e90f7318295160cf6dd05965cfde7b5ca275
RotemK1 <kalmanrotem@gmail.com>
2023-04-04 09:24:43 +0300

    modified replace

--- vars/catScript.groovy
+++ vars/catScript.groovy
@@ -1,7 +1,10 @@
 def call(Map config = [:]){
+    sh "ls"
     def scriptcontents = libraryResource "org/${config.name}"
+    sh "ls"
     sh "echo $config"
     scriptcontents = scriptcontents.replace('<REP-ONE>',"${config.replacement}")
+    sh "ls"
     writeFile file: "${config.name}", text: scriptcontents
     sh "ls"
     sh "cat ${config.name}"
